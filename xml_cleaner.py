18d4e58acbb86b48b3010de87c511a82c95657a7
boosmyller <github@greyfade.com>
2020-06-01 21:15:10 -0700

    added support for nested sequences

--- xml_cleaner.py
+++ xml_cleaner.py
@@ -1,70 +1,89 @@
 import xml.etree.ElementTree as ET
 from copy import deepcopy
 
-tree = ET.parse('sample_xmls/REEL 1_25_TEST_10 CLIPS.xml')
+tree = ET.parse('REEL 2_22.xml')
 root = tree.getroot()
 
-unique_files = [] # storage for the file element. this MAY need to exist outside this scope. not sure how this works across sequences yet. 
+unique_files = [] # storage for the file element.
 
 for track in root.findall('./sequence/media/video/track'):
     for clipitem in track.findall('clipitem'):
 
         isUnique = False
-        file_current = clipitem.find('file') # get the unique file-id
-        fileid_current = file_current.attrib['id'] # get the unique file-id
-        filename = clipitem.find('file/name') # this is more important when i need check if the file content exists
+        
+        if clipitem.find('sequence') is not None:
+            print ("found sequence")
+            clipitem_source = clipitem.find('sequence')
+            filename = clipitem.find('sequence/name') # used to check if this field has contents
+
+        elif clipitem.find('file') is not None:
+            print ("found clips")
+            clipitem_source = clipitem.find('file')
+            filename = clipitem.find('file/name') # used to check if this field has contents
+            
 
         if len(unique_files) == 0: # needs to add an item first. happens once.
-            unique_files.append(file_current)
+            unique_files.append(clipitem_source)
         else:
             for file_checking in unique_files:
-                if fileid_current == file_checking.attrib['id'] and filename is not None:
+                if clipitem_source.attrib['id'] == file_checking.attrib['id'] and filename is not None:
                     isUnique = False
-  
-                elif fileid_current == file_checking.attrib['id'] and filename is None:
-                    clipitem.remove(file_current)
+
+                elif clipitem_source.attrib['id'] == file_checking.attrib['id'] and filename is None:
+                    clipitem.remove(clipitem_source)
                     clipitem.append(file_checking)
 
                 elif filename is not None:
                     isUnique = True # this means the file is NOT in the list and needs to be added
 
         if isUnique == True: # adds file to list
-            unique_files.append(file_current)
+            unique_files.append(clipitem_source)
             isUnique = False
 
         enabled = clipitem.find('enabled')
         if (enabled.text == 'TRUE'):
             track.remove(clipitem)
 
+# need to clean this up, there's too much duping.
 for track in root.findall('./sequence/media/audio/track'):
     for clipitem in track.findall('clipitem'):
-        
-        isUnique = False
-        file_current = clipitem.find('file') # get the unique file-d
-        fileid_current = file_current.attrib['id'] # get the unique file-d
-        filename = clipitem.find('file/name') # this is more important when i need check if the file content exists
 
-        # is file present in files. Add the first, otherwise...
-        if len(unique_files) == 0: # needs to add an item first
-            unique_files.append(file_current)
+        isUnique = False
+        
+        if clipitem.find('sequence') is not None:
+            print ("found sequence")
+            clipitem_source = clipitem.find('sequence')
+            filename = clipitem.find('sequence/name') # used to check if this field has contents
+
+        elif clipitem.find('file') is not None:
+            print ("found clips")
+            clipitem_source = clipitem.find('file')
+            filename = clipitem.find('file/name') # used to check if this field has contents
+            
+
+        if len(unique_files) == 0: # needs to add an item first. happens once.
+            unique_files.append(clipitem_source)
         else:
             for file_checking in unique_files:
-                if fileid_current == file_checking.attrib['id'] and filename is not None:
+                if clipitem_source.attrib['id'] == file_checking.attrib['id'] and filename is not None:
                     isUnique = False
-                    
-                elif fileid_current == file_checking.attrib['id'] and filename is None:
-                    clipitem.remove(file_current)
+
+                elif clipitem_source.attrib['id'] == file_checking.attrib['id'] and filename is None:
+                    clipitem.remove(clipitem_source)
                     clipitem.append(file_checking)
 
                 elif filename is not None:
                     isUnique = True # this means the file is NOT in the list and needs to be added
 
         if isUnique == True: # adds file to list
-            unique_files.append(file_current)
+            unique_files.append(clipitem_source)
             isUnique = False
 
         enabled = clipitem.find('enabled')
         if (enabled.text == 'TRUE'):
             track.remove(clipitem)
 
+# remember to add the AUDIO part back in
+
+print ("writing output...")
 tree.write('sample_xmls_output/OUTPUT.xml')
